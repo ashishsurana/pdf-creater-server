d0b043f10493365598806b15e4b9255600e1ce07
Ashish Surana <ashishsurana12345@gmail.com>
2017-07-14 11:36:26 +0530

    Pdf show/download done

--- index.js
+++ index.js
@@ -19,20 +19,22 @@ app.get('/api/v1/file/:path', function (req, res, err) {
 app.post('/', function (req, res, err) {
     console.log('Info is', req.body);
     if (req.body) {
-        generatePdf(req.body.path);
+
+        generatePdf(req.body);
     }
-    res.json({ path: generatePdf(req.body) }).send();
+    res.json({ path: 'http://localhost:3000/api/v1/file/' + generatePdf(req.body) }).send();
 });
 
 function generatePdf(info) {
         doc = new PDFDocument();
-        doc.pipe( fs.createWriteStream('out1.pdf') );
+        const fileName = 'output.pdf';
+        doc.pipe( fs.createWriteStream(fileName) );
         doc.text('Name: ' + info.name ,100,100);
         doc.text('Address: ' + info.address ,100,150);
         doc.text('Phone: ' + info.phone ,100,200);
         doc.text('Email: ' + info.email ,100,250);
         doc.end();
-        return 'out.pdf';
+        return fileName;
 };
 
 app.listen(3000, function () {
