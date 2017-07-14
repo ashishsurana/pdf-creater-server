ae017391b441d5e27f3bf1d0e3bbc9d0152712ea
Ashish Surana <ashishsurana12345@gmail.com>
2017-07-14 08:46:21 +0530

    get pdf & create pdf done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+PDFDocument = require('pdfkit');
+const fs = require('fs');
+
 var app = express();
 
 // Add headers
@@ -8,16 +11,30 @@ app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.get('/', function(req, res, err) {
-    console.log('here');
-    res.send({hey:'hello world'});
+app.get('/api/v1/file/:path', function (req, res, err) {
+    console.log('here', req.params.path);
+    res.sendFile(__dirname + '/'+req.params.path);
 });
 
-app.post('/', function(req, res, err) {
+app.post('/', function (req, res, err) {
     console.log('Info is', req.body);
-    res.json({hello: "world"}).send();
+    if (req.body) {
+        generatePdf(req.body.path);
+    }
+    res.json({ path: generatePdf(req.body) }).send();
 });
+
+function generatePdf(info) {
+        doc = new PDFDocument();
+        doc.pipe( fs.createWriteStream('out1.pdf') );
+        doc.text('Name: ' + info.name ,100,100);
+        doc.text('Address: ' + info.address ,100,150);
+        doc.text('Phone: ' + info.phone ,100,200);
+        doc.text('Email: ' + info.email ,100,250);
+        doc.end();
+        return 'out.pdf';
+};
 
 app.listen(3000, function () {
-	console.log('Example app running on  3000')
+    console.log('Example app running on  3000')
 });
